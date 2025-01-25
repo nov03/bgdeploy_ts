83e9922b0e48cc46b11f7974948ebf46c0a9c05f
nov03 <ishii@serverworks.co.jp>
2025-01-25 11:30:17 +0900

    fix: accountnumber

--- bin/custom-demo.ts
+++ bin/custom-demo.ts
@@ -1,18 +1,18 @@
 import { App, Environment, StackProps } from 'aws-cdk-lib';
 import { EcsDeploymentConfig } from 'aws-cdk-lib/aws-codedeploy';
-import { Toolchain } from '../lib/toolchain'; 
-import { Constants } from '../lib/constants'; 
+import { Toolchain } from '../lib/toolchain';
+import { Constants } from '../lib/constants';
 
 
 class Demo {
     // パイプラインを構成するアカウント(TOOLCHAIN_ACCOUNT)を指定
-    private static readonly TOOLCHAIN_ACCOUNT = '968841012693';
+    private static readonly TOOLCHAIN_ACCOUNT = 'XXXXXXXX';
     private static readonly TOOLCHAIN_REGION = 'ap-northeast-1';
     // CodeCommit account is the same as the toolchain account
     public static readonly CODECOMMIT_REPO = Constants.APP_NAME;
     public static readonly CODECOMMIT_BRANCH = 'main';
     // ECSが稼働するアカウント(SERVICE_ACCOUNT)を指定
-    public static readonly SERVICE_ACCOUNT = '461233751674';
+    public static readonly SERVICE_ACCOUNT = 'YYYYYYYYY';
     public static readonly SERVICE_REGION = Demo.TOOLCHAIN_REGION;
 
     public static main() {
